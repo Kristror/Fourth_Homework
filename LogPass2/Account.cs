053c8c01f27658f5af6a719af42db740e95cc8f9
Kristror <daniil.bezy@ya.ru>
2020-12-05 17:48:05 +0500

    Added LogPass2

---
+++ LogPass2/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace LogPass2
+{
+    partial class Program
+    {
+        struct Account
+        {
+            private string login;
+            private string password;
+
+            public void Load(string path)
+            {
+                string[] str = File.ReadAllLines(path);
+                login = str[0];
+                password = str[1];
+            }
+            public bool Authorizations()
+            {
+                Console.WriteLine("Введите логин:");
+                string inLog = Console.ReadLine();
+                Console.WriteLine("Введите пароль:");
+                string inPass = Console.ReadLine();
+
+                if ((inLog == login) && (inPass == password)) return true;
+                else return false;
+            }
+        }
+    }
+}
---
+++ LogPass2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogPass2
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            ///
+            /// Домашняя работа Безукладникова Даниила
+            ///
+            /// Решить задачу с логинами из предыдущего урока, только логины и пароли считать из файла в массив.
+            /// Создайте структуру Account, содержащую Login и Password.
+            ///
+            string path = "acc.txt";
+            Account acc = new Account();
+            acc.Load(path);
+
+            int attempts = 3;
+            bool authorization = false;
+            Console.WriteLine("Программа для проверки логина и пароля");
+
+            while (attempts > 0)
+            {
+                authorization = acc.Authorizations();
+                if (authorization) break;
+                else
+                {
+                    attempts--;
+                    Console.WriteLine($"Ввенные логин и пароль не подходят осталось {attempts} попыток");
+                }
+            };
+            if (authorization) Console.WriteLine("Вы успешно вошли в систему");
+            else Console.WriteLine("Вы превысили кол-во попыток для входа в систему");
+            Console.ReadKey();
+        }
+    }
+}
