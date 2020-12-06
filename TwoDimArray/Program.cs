33d343439ba4f9c42821846034171691660d22b4
Kristror <daniil.bezy@ya.ru>
2020-12-06 14:20:54 +0500

    Added TwoDimArray

---
+++ TwoDimArray/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///
+            /// Домашняя работа Безукладникова Даниила
+            ///
+            /// *а) Реализовать класс для работы с двумерным массивом.
+            /// Реализовать конструктор, заполняющий массив случайными числами.
+            /// Создать методы, которые возвращают сумму всех элементов массива,
+            /// сумму всех элементов массива больше заданного,
+            /// свойство, возвращающее минимальный элемент массива, 
+            /// свойство, возвращающее максимальный элемент массива, 
+            /// метод, возвращающий номер максимального элемента массива(через параметры, используя модификатор ref или out)
+            /// *б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
+            /// Дополнительные задачи
+            /// в) Обработать возможные исключительные ситуации при работе с файлами.
+            ///
+            Console.WriteLine("Программа по работе с одномерным массивом");
+            Console.WriteLine("Сгенирировать новый массив (0) или открыть существующий (1)");
+            int type = 0;
+            Int32.TryParse(Console.ReadLine(), out type);
+            TwoArray arr = null;
+            if (type == 0)
+            {
+                Console.WriteLine("Генерация нового массива:");
+                Console.WriteLine("Введите высоту массива:");
+                int row = 0;
+                Int32.TryParse(Console.ReadLine(), out row);
+                Console.WriteLine("Введите ширину массива:");
+                int colls = 0;
+                Int32.TryParse(Console.ReadLine(), out colls);
+                arr = new TwoArray(row, colls, false);
+
+                Console.WriteLine("Сгенерированный массив:");
+                arr.Save("text.txt");
+            }
+            else if(type == 1)
+            {                
+                if (!TwoArray.Load(ref arr,"text.txt")) 
+                {
+                    Console.WriteLine("Не существует файла с сохранненым массивом. Выход из программы");
+                    Console.ReadKey();
+                    return;
+                }
+                Console.WriteLine("Открытый массив:");
+            }
+            arr.Print();
+            Console.WriteLine($"Сумма элементов массива: {arr.Summ()}");
+            Console.WriteLine("Введите число элементы больше которого будут использованны для вычисления суммы массива:");
+            int num = 0;
+            Int32.TryParse(Console.ReadLine(), out num);
+            Console.WriteLine($"Сумма элементов массива больше {num}: {arr.Summ(num)}");
+            Console.WriteLine($"Минимальный элемент массива: {arr.Min}"); 
+            Console.WriteLine($"Максимальный элемент массива: {arr.Max}");
+            int rowP = 0;
+            int colP = 0;
+            arr.MaxPos(ref rowP, ref colP);
+            Console.WriteLine($"Максимальный элемент массива находится в {rowP+1} строке, в {colP+1} столбце.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TwoDimArray/TwoArray.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+
+namespace TwoDimArray
+{
+    class TwoArray
+    {
+        int[,] arr;
+
+        public TwoArray(int rows, int colls, bool exist)
+        {
+            arr = new int[rows, colls];
+            if (!exist)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < colls; j++)
+                    {
+                        arr[i, j] = rnd.Next(100);
+                    }
+                }
+            }
+        }
+        public int Max
+        {
+            get
+            {
+                int max = arr[0,0];
+
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        if (arr[i, j] > max) max = arr[i, j];
+                    }
+                }
+                return max;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                int min = arr[0, 0]; 
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        if (arr[i, j] < min) min = arr[i, j];
+                    }
+                }
+                return min;
+            }
+        }
+        public int Summ()
+        {
+            int summ = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    summ += arr[i, j];
+                }
+            }
+            return summ;
+        }
+        public int Summ(int num)
+        {
+            int summ = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                   if(arr[i,j]>num) summ += arr[i, j];
+                }
+            }
+            return summ;
+        }
+
+        public void Save(string path)
+        {
+            StreamWriter sr = new StreamWriter(path,false);
+            sr.WriteLine($"{arr.GetLength(0)} {arr.GetLength(0)}");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    sr.Write($"{arr[i, j]} ");
+                }
+                sr.WriteLine();
+            }
+            sr.Close();
+        }
+
+        public static bool Load(ref TwoArray array, string path)
+        {
+            if (File.Exists(path))
+            {
+                StreamReader sr = new StreamReader(path);
+                string[] str = sr.ReadLine().Split(' ');
+                array = new TwoArray(Int32.Parse(str[0]), Int32.Parse(str[1]), true);
+                int i = 0;
+                while (!sr.EndOfStream)
+                {
+                    str = sr.ReadLine().Split(' ');
+                    for(int j  = 0; j < str.Length-1
+                        ; j++)
+                    {
+                        array.arr[i,j] = Int32.Parse(str[j]);
+                    }
+                    i++;
+                }
+                sr.Close();
+                return true;
+            }
+            else return false;
+        }
+
+        public void MaxPos(ref int row,ref int col)
+        {
+            int max = arr[0, 0];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > max)
+                    {
+                        max = arr[i, j];
+                        row = i;
+                        col = j;
+                    }
+                }
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
